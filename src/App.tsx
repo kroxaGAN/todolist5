13ef2cb71489655775da50b61914aac5206537ea
Andrei H <mwplt@mail.ru>
2021-10-14 08:31:30 +0300

    made change status Check

--- src/App.tsx
+++ src/App.tsx
@@ -47,13 +47,8 @@ function App() {
         setTasks({...tasks,[todolistID]:[newTask,...tasks[todolistID]]})
     }
 
-    function changeStatus(taskId: string, isDone: boolean) {
-        // let task = tasks.find(t => t.id === taskId);
-        // if (task) {
-        //     task.isDone = isDone;
-        // }
-        //
-        // setTasks([...tasks]);
+    function changeStatus(todolistID: string,taskId: string, isDone: boolean) {
+       setTasks({...tasks,[todolistID]:tasks[todolistID].map(m=>m.id===taskId ? {...m,isDone:isDone}:m)})
     }
 
     function changeFilter(todolistID: string, value: FilterValuesType) {
